8ded6104c7e51ce0744b0eea7c372dd54b69b0b1
Aaron A. Rich <aaron@aaronarich.com>
2018-07-02 19:24:18 -0400

    A few project updates

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "green",
+    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center"
   }
