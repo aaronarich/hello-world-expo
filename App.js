23b47cb61a41f05a407362433584fd6e78800d11
Aaron A. Rich <aaron@aaronarich.com>
2018-07-03 17:50:37 -0400

    Refactor main view to be its own component and able to scroll

--- App.js
+++ App.js
@@ -1,35 +1,12 @@
 import React from "react";
-import { StyleSheet, Text, View, TouchableHighlight } from "react-native";
+import { StyleSheet, ScrollView } from "react-native";
+
+import ColorButton from "./components/ColorButton";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
-  },
-  button: {
-    margin: 10,
-    padding: 10,
-    borderWidth: 2,
-    borderRadius: 10,
-    alignSelf: "stretch",
-    backgroundColor: "rgba(255,255,255,.8)"
-  },
-  row: {
-    flexDirection: "row",
-    alignItems: "center"
-  },
-  sample: {
-    height: 20,
-    width: 20,
-    borderRadius: 10,
-    margin: 5,
-    backgroundColor: "white"
-  },
-  text: {
-    fontSize: 30,
-    margin: 5
+    paddingTop: 48
   }
 });
 
@@ -49,29 +26,20 @@ export default class App extends React.Component {
   render() {
     const { backgroundColor } = this.state;
     return (
-      <View style={[styles.container, { backgroundColor }]}>
-        <TouchableHighlight
-          style={styles.button}
-          onPress={() => this.changeColor("yellow")}
-          underlayColor="orange"
-        >
-          <View style={styles.row}>
-            <View style={[styles.sample, { backgroundColor: "yellow" }]} />
-            <Text style={styles.text}>yellow</Text>
-          </View>
-        </TouchableHighlight>
-
-        <TouchableHighlight
-          style={styles.button}
-          onPress={() => this.changeColor("blue")}
-          underlayColor="orange"
-        >
-          <View style={styles.row}>
-            <View style={[styles.sample, { backgroundColor: "blue" }]} />
-            <Text style={styles.text}>blue</Text>
-          </View>
-        </TouchableHighlight>
-      </View>
+      <ScrollView style={[styles.container, { backgroundColor }]}>
+        <ColorButton backgroundColor="red" onSelect={this.changeColor} />
+        <ColorButton backgroundColor="blue" onSelect={this.changeColor} />
+        <ColorButton backgroundColor="green" onSelect={this.changeColor} />
+        <ColorButton backgroundColor="yellow" onSelect={this.changeColor} />
+        <ColorButton backgroundColor="red" onSelect={this.changeColor} />
+        <ColorButton backgroundColor="blue" onSelect={this.changeColor} />
+        <ColorButton backgroundColor="green" onSelect={this.changeColor} />
+        <ColorButton backgroundColor="yellow" onSelect={this.changeColor} />
+        <ColorButton backgroundColor="red" onSelect={this.changeColor} />
+        <ColorButton backgroundColor="blue" onSelect={this.changeColor} />
+        <ColorButton backgroundColor="green" onSelect={this.changeColor} />
+        <ColorButton backgroundColor="yellow" onSelect={this.changeColor} />
+      </ScrollView>
     );
   }
 }
